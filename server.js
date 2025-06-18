191ad4c28db06dc36dbfaf8ac8a3209da74344ac
hamidkhj <koohpeyma.hamid@gmail.com>
2025-06-18 15:38:50 +0100

    cors setting

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ mongoose.set('strictQuery', true);
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*',   
+    methods: '*',
+}))
 
 //db config
 mongoose.connect(process.env.MONGO_URI, {
