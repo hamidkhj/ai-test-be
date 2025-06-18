8269324e422eec99a31530ac1eb9ade846e3e2bf
hamidkhj <koohpeyma.hamid@gmail.com>
2025-06-18 15:57:40 +0100

    revert cors changes

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ mongoose.set('strictQuery', true);
 
 //middlewares
 app.use(express.json())
-app.use(cors({
-    origin: '*',   
-    methods: '*',
-}))
+app.use(cors())
 
 //db config
 mongoose.connect(process.env.MONGO_URI, {
